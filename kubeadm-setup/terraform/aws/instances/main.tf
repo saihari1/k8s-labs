b122d4d4b532e6ad66ca2af44fe0ba9e72d9d371
dev-ops-aws <128320461+dev-ops-aws@users.noreply.github.com>
2024-05-12 18:38:10 +0530

    Create main.tf

---
+++ kubeadm-setup/terraform/aws/instances/main.tf
@@ -0,0 +1,23 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+module "ec2_instance" {
+  source = "../modules/ec2"
+
+  instance_name  = "k8s-node"
+  ami_id         = var.ami_id
+  instance_type  = "t2.medium"
+  key_name       = "k8s-keypair"
+  subnet_ids     = var.subnet_id
+  instance_count = 3
+
+  inbound_from_port  = ["0", "6443", "22", "30000"]
+  inbound_to_port    = ["65000", "6443", "22", "32768"]
+  inbound_protocol   = ["TCP", "TCP", "TCP", "TCP"]
+  inbound_cidr       = ["172.31.0.0/16", "0.0.0.0/0", "0.0.0.0/0", "0.0.0.0/0"]
+  outbound_from_port = ["0"]
+  outbound_to_port   = ["0"]
+  outbound_protocol  = ["-1"]
+  outbound_cidr      = ["0.0.0.0/0"]
+}
