e8341733324162611f2c60ba57393a3c2ff8a56b
dev-ops-aws <128320461+dev-ops-aws@users.noreply.github.com>
2024-05-12 23:42:13 +0530

    Update ubuntu-box.sh

--- kubeadm-setup/vagrant-kubeadm/ubuntu-box.sh
+++ kubeadm-setup/vagrant-kubeadm/ubuntu-box.sh
@@ -1,6 +1,7 @@
 #goto https://app.vagrantup.com/boxes/search
 cd c:\vagrant-box
 vagrant box add ubuntu/trusty64
+vagrant box list
 vagrant init ubuntu/trusty64
 vagrant up
 
@@ -10,3 +11,10 @@ vargrant ssh
 #if the above doesn't work, manually connect to the machine
 vagrant ssh-config
 ssh vagrant @127.0.0.1 -p 2222 -i <identityfile>
+
+#to shutdown
+vagrant halt
+
+#to destory the bo
+vagrant destroy
+
