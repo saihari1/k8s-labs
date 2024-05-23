799de52760186cd1ec1753c101f0de1e6b8f48ea
dev-ops-aws <128320461+dev-ops-aws@users.noreply.github.com>
2024-05-23 13:13:23 +0530

    Update install.sh

--- eks-setup/eksctl-yaml/install.sh
+++ eks-setup/eksctl-yaml/install.sh
@@ -3,13 +3,11 @@ curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/d
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
 
-#install aws cli from below location
+#install aws cli from below location. kubectl requires min 2.x cli version
 # https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
 sudo yum remove awscli
 aws --version
-
-# deploy the cluster using the yaml file
-eksctl create cluster -f eks-cluster.yaml
+aws-cli/2.15.30 Python/3.11.6 Linux/5.10.205-195.807.amzn2.x86_64 botocore/2.4.5
 
 # install kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
@@ -17,6 +15,9 @@ chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
 kubectl version --client
+
+# deploy the cluster using the yaml file
+eksctl create cluster -f eks-cluster.yaml
 
 aws eks update-kubeconfig --region us-east-1 --name eks-cluster
 
