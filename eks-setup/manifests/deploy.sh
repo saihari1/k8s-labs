c2b6737075b93e36328b4b8c70bf0edf6680125d
dev-ops-aws <128320461+dev-ops-aws@users.noreply.github.com>
2024-05-23 17:41:50 +0530

    Update deploy.sh

--- eks-setup/manifests/deploy.sh
+++ eks-setup/manifests/deploy.sh
@@ -6,27 +6,30 @@ aws iam create-policy --policy-name AWSLoadBalancerControllerIAMPolicy --policy-
 
 #Then create a role with the below command
 eksctl create iamserviceaccount \
-  --cluster=<cluster-name> \
+  --cluster=eks-cluster \
   --namespace=kube-system \
   --name=aws-load-balancer-controller \
   --role-name AmazonEKSLoadBalancerControllerRole \
-  --attach-policy-arn=arn:aws:iam::<your-aws-account-id>:policy/AWSLoadBalancerControllerIAMPolicy \
-  --region=us-east-1
-  --approve
+  --attach-policy-arn=arn:aws:iam::801211930675:policy/AWSLoadBalancerControllerIAMPolicy \
+  --region=us-east-1 \
+  --override-existing-serviceaccounts \
+  --approve 
 
 helm repo add eks https://aws.github.io/eks-charts
 helpm repo update
 
 helm install aws-load-balancer-controller eks/aws-load-balancer-controller \
   -n kube-system \
-  --set clusterName=<your-cluster-name> \
+  --set clusterName=eks-cluster \
   --set serviceAccount.create=false \
   --set serviceAccount.name=aws-load-balancer-controller \
-  --set region=<region> \
-  --set vpcId=<your-vpc-id>
+  --set region=us-east-1 \
+  --set vpcId=vpc-09dadd9e423c04f91
 
 # verify
 kubectl get deployment -n kube-system aws-load-balancer-controller -w
+kubectl describe deployment aws-load-balancer-controller
+kubectl get pods -A
 
 #if you get the ingress, you can see the address
 kubectl get ingress -n game-2048-1
