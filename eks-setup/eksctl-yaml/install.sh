9f3c18ec96f961d491e1f2221c39a7f93eba54e2
saihari1 <167093570+saihari1@users.noreply.github.com>
2024-06-01 00:10:44 +0530

    Update install.sh

--- eks-setup/eksctl-yaml/install.sh
+++ eks-setup/eksctl-yaml/install.sh
@@ -1,42 +1,49 @@
-#tag the public subnets as below
-kubernetes.io/cluster/<B2B>:shared
-kubernetes.io/role/elb:1
-
-#Make sure auto-assign ip settings is enabled
-Enable auto-assign public IPv4 address is Checked
-
-
-# install eksctl
+#!/bin/bash
+# Install required packages
+sudo apt update -y
+sudo apt install -y unzip curl jq
+# Download and install AWS CLI
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+# Verify AWS CLI installation
+aws --version
+# Tag the public subnets for EKS
+aws ec2 create-tags --resources subnet-0b532be25057083a5 --tags Key=kubernetes.io/cluster/B2B,Value=shared
+aws ec2 create-tags --resources subnet-05826c870daa8606a --tags Key=kubernetes.io/role/elb,Value=1
+# Make sure auto-assign IP settings is enabled in the AWS Console for your subnets
+# Install eksctl
 curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv /tmp/eksctl /usr/local/bin
 eksctl version
-
-#install aws cli from below location. kubectl requires min 2.x cli version
-# https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-sudo yum remove awscli
-aws --version
-aws-cli/2.15.30 Python/3.11.6 Linux/5.10.205-195.807.amzn2.x86_64 botocore/2.4.5
-
-# install kubectl
+# Install kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.3/2024-04-19/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 mkdir -p $HOME/bin && cp ./kubectl $HOME/bin/kubectl && export PATH=$HOME/bin:$PATH
 echo 'export PATH=$HOME/bin:$PATH' >> ~/.bashrc
+source ~/.bashrc
 kubectl version --client
-
-# deploy the cluster using the yaml file
-eksctl create cluster -f B2B.yaml
-
+# Ensure AWS credentials and region are set correctly
+aws configure
+export AWS_REGION=ap-southeast-2
+# Verify AWS Configuration
+aws configure list
+aws sts get-caller-identity
+# Check VPC and Subnets
+aws ec2 describe-vpcs --vpc-ids vpc-0625296b14616a368
+aws ec2 describe-subnets --subnet-ids subnet-0b532be25057083a5
+aws ec2 describe-subnets --subnet-ids subnet-05826c870daa8606a
+# Deploy the cluster using the yaml file
+eksctl create cluster -f B2B.yaml --verbose 4
+# Update kubeconfig to use the new cluster
 aws eks update-kubeconfig --region ap-southeast-2 --name B2B
-
-# on Amazon Linux2 install helm as follows
+# Verify nodes and clusters
+kubectl get nodes
+# Install Helm (on Amazon Linux2)
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 helm version
-
-# to cleanup resources, run 
-#eksctl delete cluster --region=ap-southeast-2 --name=B2B
-
+# Cleanup resources (if needed)
 
 
